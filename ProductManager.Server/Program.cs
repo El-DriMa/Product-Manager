fb0e434b2823e2490a01dae20921ee7e026a95b6
Amna Hodžić <ahodzic649@outlook.com>
2024-05-15 16:40:19 +0200

    db

--- ProductManager.Server/Program.cs
+++ ProductManager.Server/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using ProductManager.Server.Data;
 using System;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,8 +17,8 @@ builder.Services.AddCors(options => options.AddPolicy(name: "FrontUI", policy =>
     policy.WithOrigins("https://localhost:4200/").AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
 }));
 
-//builder.Services.AddDbContext<AppDbContext>(options =>
-//options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
