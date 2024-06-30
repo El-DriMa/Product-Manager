151a2d243dd1799bda66d154a9be0e2b3858c478
Amna Hodžić <ahodzic649@outlook.com>
2024-06-30 23:17:57 +0200

    proizvodi

--- productmanager.client/src/app/app-routing.module.ts
+++ productmanager.client/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'product', component: ProductComponent
+  },
+  {
+    path: '', redirectTo: '/product', pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
