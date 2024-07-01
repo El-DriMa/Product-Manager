c36a95b97c70ab3211558f9da57bd772601b3222
Amna Hodžić <ahodzic649@outlook.com>
2024-07-01 21:42:03 +0200

    proizvodi prikaz

--- productmanager.client/src/app/product/product.component.ts
+++ productmanager.client/src/app/product/product.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+interface Product {
+  id: number;
+  productName: string;
+  unitPrice: number;
+  package: string;
+  isDiscontinued: boolean;
+}
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit{
+
+  public products: Product[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.http.get<Product[]>('https://localhost:7213/api/Product').subscribe(data => {
+      this.products = data;
+    }, error => { console.log("Greska pri dohvatanju podataka.", error) }
+    );
+  }
+
+
 
 }
